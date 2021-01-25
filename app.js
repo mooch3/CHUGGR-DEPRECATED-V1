ddf22c016fec5818f5770a267597b77ecaab9a7f
mooch3 <62298384+mooch3@users.noreply.github.com>
2021-01-24 19:52:03 -0800

    Update app.js

--- app.js
+++ app.js
@@ -5,18 +5,6 @@ const admin = require('firebase-admin');
 const firebase = require('firebase')
 const app = express();
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAJGkHcDPbbLW1pf29xb-uqNc9Ygd39F04",
-  authDomain: "chuggr-6a851.firebaseapp.com",
-  databaseURL: "https://chuggr-6a851.firebaseio.com",
-  projectId: "chuggr-6a851",
-  storageBucket: "chuggr-6a851.appspot.com",
-  messagingSenderId: "1046653963698",
-  appId: "1:1046653963698:web:e26ab6a28553d00f5be8ab",
-  measurementId: "G-5TMDD3N2B2"
-};
-
 firebase.initializeApp(firebaseConfig);
 
 
